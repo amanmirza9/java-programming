a645844d1e569ddb661b015369d01b420949472c
aman mirza <50958949+amanmirza9@users.noreply.github.com>
2019-05-31 01:15:45 +0500

    if else

---
+++ if_else.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class if_else {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int sal,b;
+        System.out.println("enter your salary");
+        sal=obj.nextInt();
+        if(sal>=10000)
+        {
+            b = (sal * 10) / 100;
+            sal=sal+b;
+        }
+        //System.out.println("salary is"+sal);
+        else
+        {
+            b=(sal*5)/100;
+            sal=sal+b;
+            System.out.println("salary is"+sal);
+        }
+    }
+}
