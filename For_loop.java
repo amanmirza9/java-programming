77c04acb32502f6224d516795a04fe70888cf83a
aman mirza <50958949+amanmirza9@users.noreply.github.com>
2019-05-31 08:48:10 +0500

    For_loop

---
+++ For_loop.java
@@ -0,0 +1,9 @@
+public class For_loop {
+    public static void main(String[] args) {
+        int no;
+        for(no=2; no<=100; no+=2)
+        {
+            System.out.println(no);
+        }
+    }
+}
