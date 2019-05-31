51d2ecfec91584f2bf70f3fbbc83e0811dbe2616
aman mirza <50958949+amanmirza9@users.noreply.github.com>
2019-05-31 08:45:34 +0500

    while_statement

---
+++ while_statement.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class while_statement {
+    public static void main(String[] args) {
+        int no=1;
+        while(no<100)
+        {
+            System.out.println(no);
+            no=no+2;
+
+        }
+    }
+}
