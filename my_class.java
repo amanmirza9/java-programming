260641aaf4d278b15f9f6f7e34bbb0c4505f8abf
aman mirza <50958949+amanmirza9@users.noreply.github.com>
2019-07-10 22:33:48 +0500

    Add files via upload

---
+++ my_class.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+public class my_class {
+    public static void main(String[] args) {
+        boolean bool=true;
+        byte by=1;
+        String c="sajjad";
+        short s =32767;
+        int i=5;
+        long l=300l;
+        float f =123f;
+        double d=5.0;
+        System.out.println("all types");
+        System.out.println("bool"+bool);
+        System.out.println("byte"+by);
+        System.out.println("short"+s);
+        System.out.println("long"+l);
+        System.out.println("float"+f);
+        System.out.println("double"+d);
+        System.out.println("string"+c);
+
+       /* String s0=new String("coding");
+        String s1="is";
+        String s2="awesome";
+        String s3=new String("hello");
+        String s4=new String("hello");
+        String s5="hello";
+        System.out.println(s0+s1+s2);
+        System.out.println(s3==s4);
+        System.out.println(s3.equals(s4));
+        System.out.println(s5=="hello");//*/
+
+       /* Map<String,Integer>size =new HashMap<>();
+        size.put("alice",171);
+        size.put("bob",182);
+        for(String name:size.keySet()){
+            System.out.println(name+"is"+size.get(name)+"cm tall");
+        }
+*/
+
+
+    }
+}
