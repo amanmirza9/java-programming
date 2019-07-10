925d6107cd287fe5f1ea35c54890f9576a1c47bb
aman mirza <50958949+amanmirza9@users.noreply.github.com>
2019-07-10 22:15:27 +0500

    Add files via upload

---
+++ reverse_for_loop.java
@@ -0,0 +1,8 @@
+public class reverse_for_loop {
+    public static void main(String[] args) {
+        for(int i=10; i>0;i--)
+        {
+            System.out.println("your loop="+i);
+        }
+    }
+}
