0f70e083cfd4f8b5fc4a71d1f87b484e69650c77
aman mirza <50958949+amanmirza9@users.noreply.github.com>
2019-07-10 22:13:06 +0500

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class task1 {
+    public static void main(String[] args) {
+        int r = 1, star;
+        while (r <= 3) {
+            System.out.print("*");
+            star =  + 1;
+        }
+        System.out.println("*");
+        //// += 1;
+    }
+
+    }
